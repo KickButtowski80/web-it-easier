1f5f81cb0a7af8eabdf3d93b966e28be1d956951
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-24 14:19:59 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template lang="">  
+   <Menu></Menu>
+</template>
+<script>
+import Menu from './components/Menu.vue';
+export default {
+  components:{
+    Menu,
+  }
+};
+</script>
+<style lang=""></style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template lang="">
+ <section class="bg-blue-200">
+    <div class="">IT Portfolio</div>
+ </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
