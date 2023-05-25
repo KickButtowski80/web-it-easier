f41d8013018c715225b3adce21c260562a6eed17
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-25 15:34:15 -0700

    register hero cmp

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template lang="">  
    <Menu></Menu>
-   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eligendi ex ipsa quae dolore hic, illum exercitationem reiciendis sit, quia nemo aliquam expedita est laboriosam commodi assumenda temporibus nulla saepe!
-   Enim neque excepturi libero dolore saepe accusamus commodi suscipit consequatur quae, laboriosam, nisi beatae. Quaerat blanditiis iste nisi numquam reprehenderit ea enim quasi esse, tenetur necessitatibus ipsum odit totam doloribus!
-   Ducimus repellat quidem neque voluptas itaque, sit dignissimos minus, animi aspernatur, at temporibus ea. Doloribus iste cupiditate eligendi, cumque nemo provident omnis molestiae ea, soluta unde architecto quo, non totam.
-   Nesciunt libero, aperiam architecto praesentium sed vel voluptatum doloremque! Nemo enim quaerat tenetur. Cum dolor laudantium, temporibus quis quod, ipsam perspiciatis numquam earum, dolorem perferendis vero! Facilis eum voluptates pariatur?
-   Officiis doloribus laborum porro ad distinctio qui autem facilis voluptate non et amet cupiditate corrupti a, facere harum similique maiores labore vel aliquam ullam? Quibusdam, veritatis. Magnam earum fugit iste.
-   Repellat vitae itaque obcaecati magnam veritatis aliquam assumenda quos suscipit molestiae laudantium adipisci culpa maiores quod, ducimus mollitia consequuntur iure voluptas doloremque, quibusdam eveniet excepturi! Eum sequi iure totam culpa?
-   Accusamus sequi aliquid, vel magnam laborum impedit deserunt temporibus nisi porro dicta ipsam sapiente totam, sed nobis. Nemo atque aliquid eaque ad voluptatum velit fugit incidunt a! Accusamus, dolor sint?
-   Delectus vitae mollitia reiciendis minima! Eligendi commodi sapiente ipsam sed dolorum consectetur quas consequatur natus exercitationem aliquid, inventore, explicabo voluptatum nulla repellendus labore libero cumque perferendis ullam odio, corporis eum?
+   <Hero/>
+   <p class="max-w-2xl mx-auto">
+
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eligendi ex ipsa quae dolore hic, illum exercitationem reiciendis sit, quia nemo aliquam expedita est laboriosam commodi assumenda temporibus nulla saepe!
+     Enim neque excepturi libero dolore saepe accusamus commodi suscipit consequatur quae, laboriosam, nisi beatae. Quaerat blanditiis iste nisi numquam reprehenderit ea enim quasi esse, tenetur necessitatibus ipsum odit totam doloribus!
+     Ducimus repellat quidem neque voluptas itaque, sit dignissimos minus, animi aspernatur, at temporibus ea. Doloribus iste cupiditate eligendi, cumque nemo provident omnis molestiae ea, soluta unde architecto quo, non totam.
+     Nesciunt libero, aperiam architecto praesentium sed vel voluptatum doloremque! Nemo enim quaerat tenetur. Cum dolor laudantium, temporibus quis quod, ipsam perspiciatis numquam earum, dolorem perferendis vero! Facilis eum voluptates pariatur?
+     Officiis doloribus laborum porro ad distinctio qui autem facilis voluptate non et amet cupiditate corrupti a, facere harum similique maiores labore vel aliquam ullam? Quibusdam, veritatis. Magnam earum fugit iste.
+     Repellat vitae itaque obcaecati magnam veritatis aliquam assumenda quos suscipit molestiae laudantium adipisci culpa maiores quod, ducimus mollitia consequuntur iure voluptas doloremque, quibusdam eveniet excepturi! Eum sequi iure totam culpa?
+     Accusamus sequi aliquid, vel magnam laborum impedit deserunt temporibus nisi porro dicta ipsam sapiente totam, sed nobis. Nemo atque aliquid eaque ad voluptatum velit fugit incidunt a! Accusamus, dolor sint?
+     Delectus vitae mollitia reiciendis minima! Eligendi commodi sapiente ipsam sed dolorum consectetur quas consequatur natus exercitationem aliquid, inventore, explicabo voluptatum nulla repellendus labore libero cumque perferendis ullam odio, corporis eum?
+   </p>
 </template>
 <script>
 import Menu from './components/Menu.vue';
+import Hero from './components/Hero.vue'
 export default {
   components:{
     Menu,
+    Hero,
   }
 };
 </script>
