8679e452b39cc0e761782d4af33ab1a5450aa594
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-25 15:34:05 -0700

    hero cmp

---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template lang="">
+<section class="bg-purple-600 min-h-screen md:h-50">
+    <img class="h-[30vh] rounded-full  bg-purple-600" src="/it.png" alt="">
+</section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
