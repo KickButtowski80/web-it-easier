399008390ddc280544fa3d92623f2a73195a8048
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-25 14:15:37 -0700

    Top Menu Cmp

---
+++ src/components/Menus/TopMenu.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <section class="bg-purple-400  ">
+    <div class="p-2 flex max-w-2xl mx-auto relative">
+      <span class="text-xl md:text-2xl"> IT 🚪 </span>
+      <nav
+        class="hidden md:[&>*]:pr-5 md:[&>*]:text-xl
+         md:[&>*]:font-semibold md:flex md:absolute md:right-0"
+        aria-label="main"
+      >
+        <a href="#">Home</a>
+        <a href="#">My Work</a>
+        <a href="#">Hire me</a>
+      </nav>
+
+    </div>
+  </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
