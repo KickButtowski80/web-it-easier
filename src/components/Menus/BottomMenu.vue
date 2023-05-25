028635398ddf599c7dc08de2b3d1976f695d03c3
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-25 14:12:40 -0700

    Bottom Menu Cmp

---
+++ src/components/Menus/BottomMenu.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <section>
+        <nav 
+          :class="{hidden: hideIt}"
+          class="bg-purple-900 [&>*]:text-white flex justify-around
+                    fixed w-full bottom-0 h-10">
+          <a href="#">Home</a>
+          <a href="#">My Work</a>
+          <a href="#">Hire me</a>
+        </nav>
+    </section>
+</template>
+<script>
+export default {
+    props: ['hideIt']
+
+}
+</script>
+<style lang="">
+    
+</style>
