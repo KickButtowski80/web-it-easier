afd2866ce2ed320e3ad141ff9e29e6e295d75f4c
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-26 10:27:00 -0700

    img and slogan in hero cmp

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,24 @@
 <template lang="">
-<section class="bg-purple-600 min-h-screen md:h-50">
-    <img class="aspect-[4/3] rounded-full  bg-purple-600" src="/it.png" alt="">
-</section>
+  <section class="bg-purple-500 h-screen md:h-[%50]
+                 flex flex-col md:flex-row-reverse
+                 md:justify-evenly">
+    <img
+      class="w-64 md:w-1/2 md:aspect-[2/2] mx-auto bg-purple-500 pt-20"
+      src="/it-min.png"
+      alt=""
+    />
+    <p class="max-w-xl mx-auto text-3xl md:text-5xl
+              font-semibold text-white mt-24 leading-loose md:leading-[6rem]
+              text-center md:flex md:items-center">
+      The door 🚪
+      <br />
+      to an easier 🚀
+      <br />
+      future ▶️
+    </p>
+  </section>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
