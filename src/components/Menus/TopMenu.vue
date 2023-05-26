87614fc6bad71bdb2be79bf5c140221a3a17c7e9
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-25 18:45:46 -0700

    sticky top menu

--- src/components/Menus/TopMenu.vue
+++ src/components/Menus/TopMenu.vue
@@ -1,6 +1,6 @@
 <template lang="">
-  <section class="bg-purple-400  ">
-    <div class="p-2 flex md:items-center max-w-2xl mx-auto relative">
+  <section class="bg-purple-400 sticky top-0 ">
+    <div class="p-2 flex md:items-center  relative">
       <span class="text-xl font-semibold md:text-7xl"> IT 🚪 </span>
       <nav
         class="hidden md:[&>*]:pr-5 md:[&>*]:text-4xl md:[&>*]:p-2
