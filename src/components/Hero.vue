efc33c683fb288168bb267bdf81ebc399da325fa
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-30 18:41:08 -0700

    animation the logo in hero cmp

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,25 @@
 <template lang="">
-  <section class="bg-purple-500 h-screen md:h-[%50]
-                 flex flex-col md:flex-row-reverse
-                 md:justify-evenly md:py-20" >
-    <img
+  <section class="bg-purple-500 h-screen
+                  text-white
+                 flex flex-col items-center
+                 md:flex-row
+                
+                 " >
+    <!-- <img
       class=" w-64 sm:w-72 md:w-96  md:aspect-[13/12]
        mx-auto bg-purple-500 md:pt-20 rounded-full
       "
       src="/it-min.png"
       alt=""
-    />
-    <p class="max-w-xl mx-auto text-3xl md:text-5xl mt-5 md:mt-20
-              font-semibold text-white md:leading-[6rem]
-              text-center md:flex md:items-center">
+    /> -->
+    <div class="font-bold text-[9rem] relative">
+      <div class="inline-block
+                  animate-rotate-letter-right">I</div>
+      <div class="inline-block animate-rotate-letter-left ">T</div>
+    </div>
+    <p class="max-w-xl mx-auto text-3xl md:text-5xl
+              font-semibold  md:leading-[6rem]
+              text-center ">
       The door 🚪
       <br />
       to an easier ...🚀
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,48 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaley(0)' },
+          '80%': { transform: 'scaley(1.2)' },
+          '100%': { transform: 'scaley(1)' },
+        },
+        'close-menu': {
+          '0%': { transform: 'scaley(1)' },
+          '80%': { transform: 'scaley(1.2)' },
+          '100%': { transform: 'scaley(0)' },
+        },
+        'rotate-letter-right': {
+          '0%': { 
+            'transform-origin': 'bottom center',
+            opacity: 0,
+            transform: 'rotate(-90deg)'
+                 },
+     
+          '100%': {
+             transform:'rotate(0deg)',
+             },
+        },
+        'rotate-letter-left': {
+          '0%': { 
+            'transform-origin': 'bottom center',
+            opacity: 0,
+            transform: 'rotate(90deg)'
+                 },
+     
+          '100%': {
+             transform:'rotate(0deg)',
+             },
+        }
+      },
+      animation: {
+        'open-menu': 'open-menu 2s ease-in-out forwards',
+        'close-menu': 'close-menu 0.5s ease-in-out forwards',
+        'rotate-letter-right': 'rotate-letter-right 1s ease forwards',
+        'rotate-letter-left':'rotate-letter-left 1s ease forwards'
+      }
+    },
   },
   plugins: [],
 };
