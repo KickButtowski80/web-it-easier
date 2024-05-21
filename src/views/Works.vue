302c4711fd4e00e3e4d9946cb6f2719e42afd952
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-21 16:01:54 -0700

    add work component sample to works view

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,5 +1,16 @@
 <template>
     <section class="min-h-screen">
         this is work section
+        <Work/>
     </section>
 </template>
+
+<script>
+import Work from '../components/Work.vue'
+
+export default{
+    components:{
+        Work
+    }
+}
+</script>
