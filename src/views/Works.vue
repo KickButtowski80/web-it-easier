8c2e9c90a0a358a9cbd8193943f9f4ed5d71fefc
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-23 13:54:47 -0700

    passing freelance project history to child component

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,16 +1,33 @@
 <template>
-    <section class="min-h-screen">
-        this is work section
-        <Work/>
-    </section>
+  <h1 class="text-center font-bold py-6 text-3xl mt-20">My Work</h1>
+  <section
+    class="min-h-screen grid grid-cols-1 gap-y-6 place-items-center md:grid-cols-2 lg:grid-cols-3"
+  >
+  <div v-for="project in freelancerProjectHistory" :key="project.projectId">
+    <Work :projectInfo="project"/>
+  </div>
+  </section>
 </template>
 
 <script>
-import Work from '../components/Work.vue'
+import { ref, onMounted } from 'vue';
+import freelancerProjectHistoryData from '../assets/json/freelancerProjectHistoryData.json';
+import Work from '../components/Work.vue';
 
-export default{
-    components:{
-        Work
-    }
-}
+export default {
+  components:{
+    Work
+  },
+  setup() {
+    const freelancerProjectHistory = ref([]);
+
+    onMounted(() => {
+      freelancerProjectHistory.value = freelancerProjectHistoryData.freelancerProjectHistory;
+    });
+
+    return {
+      freelancerProjectHistory,
+    };
+  },
+};
 </script>
