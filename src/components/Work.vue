08d32f6af00e0e26a5486ac73b21f702c0028988
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-27 17:04:49 -0700

    add details tag which is collapseable with smart status indicator on the btn shows read less or more Fixes #4

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,72 +1,89 @@
 <template>
- <div class="flex flex-wrap min-h-[70rem] justify-center  shadow-md">
-  <div class="max-w-sm mx-2 rounded overflow-hidden shadow-lg bg-white flex flex-col">
-    <img
-      class="w-full h-64 object-cover border rounded-lg border-purple-800 my-5"
-      :src="image"
-      :title="imageAlt"
-      :alt="imageAlt"
-    />
-    <div class="gray-bg-card flex-grow">
-      <div class="font-bold text-xl mb-2 text-blue-600">{{ projectTitle }}</div>
-      <p class="text-gray-700 text-base">
-        {{ description }}
-      </p>
-    </div>
-    <div class="px-3 pt-4 pb-2">
-      <span
-        v-for="(tec, index) in technologiesUsed"
-        :key="index"
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-      >
-        #{{ tec }}
-      </span>
-      <p class="text-gray-900 leading-none mt-2">
-        <span class="font-bold">Role:</span> Developer
-      </p>
-      <p class="text-gray-600">
-        <span class="font-bold">Completed:</span> January 2024
-      </p>
-    </div>
-    <div class=" gray-bg-card">
-      <h3 class="font-bold text-lg mb-2 text-blue-600">Highlights</h3>
-      <ul class="list-disc pl-5 space-y-1 text-gray-600">
-        <li v-for="(highlight, index) in highlights" :key="index">
-          {{ highlight }}
-        </li>
-      </ul>
-    </div>
-    <div class="px-3 py-4 flex gap-2">
-      <a
-        class="inline-block align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-blue-600 text-white shadow-md hover:shadow-lg hover:bg-blue-700 focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none"
-        type="button"
-        :href="liveView"
-        target="_blank"
-      >
-        Live View
-      </a>
-      <a
-        class="inline-block align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-green-600 text-white shadow-md hover:shadow-lg hover:bg-green-700 focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none"
-        type="button"
-        :href="codeView"
-        target="_blank"
-      >
-        Code View
-      </a>
+  <div class="flex flex-wrap justify-center shadow-md">
+    <div
+      class="max-w-sm mx-2 rounded overflow-hidden shadow-lg bg-white flex flex-col"
+    >
+      <img
+        class="w-full h-64 object-cover border rounded-lg border-purple-800 my-5"
+        :src="image"
+        :title="imageAlt"
+        :alt="imageAlt"
+      />
+      <div class="font-bold text-xl mb-2 text-blue-600 text-center">{{ projectTitle }}</div>
+      <details class="">
+        <summary
+          class="font-bold text-xl mb-2 text-blue-600 flex justify-center items-center"
+        >
+          <!-- {{ projectTitle }} -->
+          <div
+            @click="toggleReadMoreStatus"
+            class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 flex item-center w-fit"
+          >
+            {{ readMoreText }}
+          </div>
+        </summary>
+
+        <div class="gray-bg-card flex-grow">
+          <!-- <div class="font-bold text-xl mb-2 text-blue-600">{{ projectTitle }}</div> -->
+          <p class="text-gray-700 text-base">
+            {{ description }}
+          </p>
+        </div>
+        <div class="px-3 pt-4 pb-2">
+          <span
+            v-for="(tec, index) in technologiesUsed"
+            :key="index"
+            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            #{{ tec }}
+          </span>
+          <p class="text-gray-900 leading-none mt-2">
+            <span class="font-bold">Role:</span> Developer
+          </p>
+          <p class="text-gray-600">
+            <span class="font-bold">Completed:</span> January 2024
+          </p>
+        </div>
+        <div class="gray-bg-card">
+          <h3 class="font-bold text-lg mb-2 text-blue-600">Highlights</h3>
+          <ul class="list-disc pl-5 space-y-1 text-gray-600">
+            <li v-for="(highlight, index) in highlights" :key="index">
+              {{ highlight }}
+            </li>
+          </ul>
+        </div>
+        <div class="px-3 py-4 flex gap-2">
+          <a
+            class="inline-block align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-blue-600 text-white shadow-md hover:shadow-lg hover:bg-blue-700 focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none"
+            type="button"
+            :href="liveView"
+            target="_blank"
+          >
+            Live View
+          </a>
+          <a
+            class="inline-block align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-green-600 text-white shadow-md hover:shadow-lg hover:bg-green-700 focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none"
+            type="button"
+            :href="codeView"
+            target="_blank"
+          >
+            Code View
+          </a>
+        </div>
+      </details>
     </div>
   </div>
-</div>
-
 </template>
 
 <script>
-import { toRefs, toRef } from "vue";
+import { toRefs, toRef, ref, computed } from "vue";
 
 export default {
   props: {
     projectInfo: Object,
   },
-  setup(props) { 
+  setup(props) {
+    const readMoreStatus = ref("more");
     if (props.projectInfo) {
       const {
         projectId,
@@ -80,8 +97,17 @@ export default {
         endDate,
         highlights,
         liveView,
-        codeView
+        codeView,
       } = toRefs(props.projectInfo);
+
+      const readMoreText = computed(() => {
+        return readMoreStatus.value === "more" ? "Read More" : "Read Less";
+      });
+      const toggleReadMoreStatus = () => {
+        readMoreStatus.value =
+          readMoreStatus.value === "more" ? "less" : "more";
+      };
+
       return {
         projectId,
         image,
@@ -94,9 +120,38 @@ export default {
         endDate,
         highlights,
         liveView,
-        codeView
+        codeView,
+        readMoreText,
+        readMoreStatus,
+        toggleReadMoreStatus,
       };
     }
   },
 };
 </script>
+
+<style scoped>
+summary {
+  list-style: none; /* Remove the default list-style */
+  cursor: pointer; /* Optional: change the cursor to pointer */
+  margin-block: 1rem;
+  margin-inline: 1rem;
+}
+
+details[open] > summary::before {
+  display: none; /* Hide the default triangle when the details element is open */
+}
+
+details summary::-webkit-details-marker {
+  display: none; /* Hide the default triangle in WebKit browsers (Chrome, Safari) */
+}
+
+/* details summary ~ * {
+  animation: sweep .5s ease-in-out;
+}
+
+@keyframes sweep {
+  0%    {opacity: 0; margin-left: -10px}
+  100%  {opacity: 1; margin-left: 0px}
+} */
+</style>
