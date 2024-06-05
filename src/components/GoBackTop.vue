95d4449d6d176a4500c16e17a7d6d7283a2ee050
kickbuttowski80 <pazpaz25@gmail.com>
2024-06-05 16:32:42 -0700

    add mouse leave event to have better user experience

--- src/components/GoBackTop.vue
+++ src/components/GoBackTop.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="doorgroup" @click="scrollToTop" aria-label="Go to top of page">
+    <div class="doorgroup" @click="scrollToTop"
+                     @mouseleave="scrollToTop" aria-label="Go to top of page">
       <div class="doorway" ref="doorWay">
         <div id="openDoor" class="door" ref="door">
           <div
