bdefa15780c7cbc203d356b5d1257c804985fef6
kickbuttowski80 <pazpaz25@gmail.com>
2024-06-10 17:45:56 -0700

    rid of destructing

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { viteCompression } from "vite-plugin-compression";
+import  viteCompression  from "vite-plugin-compression";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
