309daaef0d1c5ef75e57becc86d7ac4fce6720d2
kickbuttowski80 <pazpaz25@gmail.com>
2024-06-10 17:29:20 -0700

    apply vite-plugin-compression for compress text-based assets

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
+import compression  from "vite-plugin-compression";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression ({
+      ext: '.gz', // Output compressed files with .gz extension
+      filter: /\.(js|css|html|svg)$/i, // Compress all text-based assets
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  server: {
+  server: { 
     watch: {
       usePolling: true,
     },
