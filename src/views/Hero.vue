9505f4be4f8e926a25d38094dcce68b77fa42d88
kickbuttowski80 <pazpaz25@gmail.com>
2024-06-17 14:24:22 -0700

    add mt to replay animation

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -62,7 +62,11 @@
     <button
     type="button"
       @click="replayAnimation"
-      class=" ml-6 -translate-y-28 z-1 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 flex item-center w-fit"
+      class="mt-2 ml-6 -translate-y-28 z-1 focus:outline-none text-white
+       bg-purple-700 hover:bg-purple-800 focus:ring-4
+        focus:ring-purple-300 font-medium rounded-lg text-sm 
+        px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700
+         dark:focus:ring-purple-900 flex item-center w-fit"
       role="button"
     >
       Replay Animation
