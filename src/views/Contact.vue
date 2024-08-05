b9e731702d83ea7dff98887b5f61feff3ce475e8
kickbuttowski80 <pazpaz25@gmail.com>
2024-08-05 14:00:38 -0700

    design an idea for non mobile view Fixes #33

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,30 @@
 <template>
-  <section class="grid place-items-center">
+  <section class="grid place-items-center min-h-screen">
     <div id="contact"></div>
     <h1 class="text-center font-bold py-6 text-3xl mt-20">Hire Us</h1>
     <div
-      class="block max-w-sm md:max-w-3xl p-6 mb-16 md:mb-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+      class="relative grid grid-cols-2 max-w-sm md:max-w-3xl p-6 mb-16 md:mb-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
     >
-      <h2 class="text-center font-semibold text-2xl text-[#146cba]">
-        the place to make your dream a reality
-      </h2>
-      <p class="font-normal text-gray-700 pb-32">
-        Here are the biggest enterprise technology acquisitions of 2024 so far,
-        in reverse chronological order.
-      </p>
+      <div class="cols-span-1"></div>
+      <div>
+        <h2 class="text-center font-semibold text-2xl text-[#146cba]">
+          the place to make your dream a reality
+        </h2>
+        <p class="font-normal text-gray-700 pb-32">
+          Here are the biggest enterprise technology acquisitions of 2024 so
+          far, in reverse chronological order.
+        </p>
+      </div>
+      <div
+        id="hire-us-info"
+        class="max-w-sm bg-blue-600 p-6 rounded-xl
+         absolute bottom-9 -left-9 min-h-full"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea facere
+        quaerat ipsam soluta reprehenderit, nesciunt quos nobis suscipit
+        consequatur sit voluptas maiores ab tenetur illum obcaecati, dolor nulla
+        provident fuga!
+      </div>
     </div>
   </section>
 </template>
