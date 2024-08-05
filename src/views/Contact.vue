0ed60c35b41f62cc21a927d441b0eee0e64f37d6
kickbuttowski80 <pazpaz25@gmail.com>
2024-08-05 00:49:46 -0700

    play with hire us section

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,17 @@
 <template>
-    <section class="min-h-screen">
-        <div id="contact"></div>
-        <h1 class="text-center font-bold py-6 text-3xl mt-20">Hire Us</h1>
-        <h2 class="text-center font-semibold text-2xl ">the place to make your dream a reality </h2>
-        
-    </section>
+  <section class="grid place-items-center">
+    <div id="contact"></div>
+    <h1 class="text-center font-bold py-6 text-3xl mt-20">Hire Us</h1>
+    <div
+      class="block max-w-sm md:max-w-3xl p-6 mb-16 md:mb-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+    >
+      <h2 class="text-center font-semibold text-2xl text-[#146cba]">
+        the place to make your dream a reality
+      </h2>
+      <p class="font-normal text-gray-700 pb-32">
+        Here are the biggest enterprise technology acquisitions of 2024 so far,
+        in reverse chronological order.
+      </p>
+    </div>
+  </section>
 </template>
