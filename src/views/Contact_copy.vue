11b7618ad0c72e16536ad8404320049665cab6aa
kickbuttowski80 <pazpaz25@gmail.com>
2024-08-06 00:10:29 -0700

    fix scrolling to top issue Fixes #33

--- src/views/Contact_copy.vue
+++ src/views/Contact_copy.vue
@@ -1,12 +1,11 @@
 <template>
-  <section
-    class="grid place-items-center min-h-screen1 bg-[url('/layered-steps-haikei-bg.svg')] bg-cover bg-center h-full w-full"
-  >
-    <header class="text-center mb-20 mt-10">
-      <div id="contact" aria-label="Contact Section"></div>
-      <h1 class="font-bold py-6 text-3xl">Hire Us</h1>
+  <section  class="grid place-items-center min-h-screen bg-[url('/layered-steps-haikei-bg.svg')] bg-cover bg-center h-full w-full">
+
+      <div id="contact"></div>
+      <header class="text-center mb-20 mt-10 font-bold py-6 text-3xl">
+      Hire Us
     </header>
-    <article
+    <div
       class="relative md:left-9 grid grid-cols-1 md:grid-cols-2 max-w-sm md:max-w-xl lg:max-w-3xl p-6 mb-16 md:mb-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 glass"
     >
       <div class="col-span-1"></div>
@@ -19,26 +18,15 @@
           far, in reverse chronological order.
         </p>
       </section>
-      <aside
+      <section
         id="hire-us-info"
-        class="max-w-sm bg-purple-400 p-6 rounded-xl md:absolute relative bottom-[-20%] md:bottom-9 md:-left-28 min-h-full text-blue-700"
+        class="max-w-sm bg-purple-400 p-6 rounded-xl absolute -bottom-full md:bottom-9 md:-left-28 min-h-full text-blue-700"
       >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea facere
         quaerat ipsam soluta reprehenderit, nesciunt quos nobis suscipit
         consequatur sit voluptas maiores ab tenetur illum obcaecati, dolor nulla
         provident fuga!
-      </aside>
-    </article>
+      </section>
+    </div>
   </section>
 </template>
-<style scoped lang="css">
-.glass {
-  backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px); /* For Safari */
-  background-color: rgba(255, 255, 255, 0.3); /* Adjust the transparency */
-  border-radius: 1rem; /* Rounded corners */
-  padding: 1.5rem; /* Padding */
-  color: #fff; /* Text color */
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Optional: Add a subtle shadow */
-}
-</style>
