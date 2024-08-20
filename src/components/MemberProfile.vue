7fe6970f7d0c882e64b18dc63407c92ced1d2b1c
kickbuttowski80 <pazpaz25@gmail.com>
2024-08-20 15:29:11 -0700

    Fix details summary tags Fixes #33

--- src/components/MemberProfile.vue
+++ src/components/MemberProfile.vue
@@ -8,35 +8,27 @@
     />
 
     <figcaption>
-      <summary>
-        <h2 class="text-black text-xl font-bold">{{ name }}</h2>
-        <h3 class="text-gray-700 text-lg font-semibold">{{ title }}</h3>
-        <details>
-          <div class="text-justify mb-3">
-            {{ bio }}
-          </div>
-        </details>
-      </summary>
+      <details>
+        <summary>
+          <h2 class="text-black text-xl font-bold">{{ fullName }}</h2>
+          <h3 class="text-gray-700 text-lg font-semibold">{{ title }}</h3>
+        </summary>
+        <div class="text-justify mb-3">
+          {{ bio }}
+        </div>
+      </details>
     </figcaption>
   </figure>
 </template>
 <script>
-
 import { toRefs, toRef, ref, computed, nextTick } from "vue";
 export default {
   props: {
     memberInfo: Object,
   },
   setup(props) {
+    const { image, imgAlt, fullName, title, bio } = toRefs(props.memberInfo);
 
-      const { 
-        image,
-        imgAlt,
-        fullName,
-        title,
-        bio,
-         } = toRefs(props.memberInfo);
-   
     return {
       image,
       imgAlt,
