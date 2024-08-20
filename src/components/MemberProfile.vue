08fa6ecb221908711bc7bb62382352727e227c9e
kickbuttowski80 <pazpaz25@gmail.com>
2024-08-20 12:22:59 -0700

    make the member profile

---
+++ src/components/MemberProfile.vue
@@ -0,0 +1,29 @@
+<template>
+     <figure class="clear-both">
+          <img
+            src="/programmers/skman-tyler.svg"
+            alt="picture of Samuel Tyler, lead developer "
+            class="rounded-full float-left"
+            style="shape-outside: circle()"
+          />
+
+          <figcaption>
+            <summary>
+              <h2 class="text-black text-xl font-bold">Samuel C. Tyler</h2>
+              <h3 class="text-gray-700 text-lg font-semibold">Team Lead</h3>
+              <details>
+                <div class="text-justify mb-3">
+                  Web Application Developer with 10+ years experience in modern,
+                  full-stack, component-based design with a focus on reliability
+                  via automated testing. Extremely diverse history in getting up
+                  to speed quickly and utilizing many different technologies to
+                  solve real-world problems. Diverse portfolio of applications
+                  includes an online document viewer, inventory management
+                  software, online health care tracking apps, and social network
+                  visualization and analysis.
+                </div>
+              </details>
+            </summary>
+          </figcaption>
+        </figure>
+</template>
