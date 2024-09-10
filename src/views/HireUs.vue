53f086b0f8920cc6f943aad153e5e7edf345cf82
kickbuttowski80 <pazpaz25@gmail.com>
2024-09-09 17:29:46 -0700

    set contact info to appear behind the team members cards

--- src/views/HireUs.vue
+++ src/views/HireUs.vue
@@ -2,67 +2,82 @@
   <section
     id="hire-us"
     aria-label="Hire Us Section"
-    class="grid place-items-center"
+    class="grid place-items-center "
   >
     <header class="text-center mb-20">
       <div></div>
       <h1 class="font-bold py-6 text-3xl">Hire Us</h1>
     </header>
-    <section
-      class="hire-us-article relative md:left-9 grid grid-cols-2
-       md1:grid-cols-2 max-w-sm md:max-w-xl lg:max-w-3xl p-6
-        mb-16 md:mb-8 bg-white border border-gray-200
-         rounded-lg shadow hover:bg-gray-100 glass"
-    >
-      <div class="col-span-1"></div>
-      <section class="col-span-1">
-        <div class="card members-info">
-          <div id="contact" class="font-normal text-gray-900">
-            <h2 class="text-center font-semibold text-2xl text-blue-700">
-              Contact Us to make your dream a reality
-            </h2>
 
-            <div class="flex flex-col">
-              <div>Tel: <br>
-                <a href="tel:+1 (424)-442-0591">(424)-442-0591 </a></div>
-              <div>
-                Email:
-                <a href="mailto:pazpaz25@gmail.com">pazpaz25@gmail.com</a>
+<!-- 
+      <section
+        class="contact-info absolute md:left-9 grid grid-cols-2
+         md1:grid-cols-2 max-w-sm md:max-w-xl lg:max-w-3xl p-6
+          mb-16 md:mb-8 bg-white border border-gray-200
+           rounded-lg shadow hover:bg-gray-100 glass"
+      >
+        <div class="col-span-1"></div>
+        <section class="col-span-1">
+          <div class="card">
+            <div  class="font-normal text-gray-900">
+              <h2 class="text-center font-semibold text-2xl text-blue-700">
+                Contact Us to make your dream a reality
+              </h2>
+              <div class="flex flex-col">
+                <div>Tel: <br>
+                  <a href="tel:+1 (424)-442-0591">(424)-442-0591 </a></div>
+                <div>
+                  Email:
+                  <a href="mailto:pazpaz25@gmail.com">pazpaz25@gmail.com</a>
+                </div>
               </div>
             </div>
           </div>
-
-          <!-- <div id="izak-info" class="member">
-            <a href="#close"> X </a>
-            <h1>Izak Info</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-              velit magni illo deserunt iusto voluptatem rem cumque
-              necessitatibus eius tempora. Corporis, modi. Nisi quia iusto
-              mollitia ex porro esse recusandae?
-            </p>
-          </div>
-          <div id="sam-info" class="member">
-            <a href="#close"> X </a>
-            <h1>Sam Info</h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
-              voluptatem deserunt hic maxime. Doloribus exercitationem
-              distinctio veritatis voluptatibus, adipisci amet possimus ullam
-              dolores facere rem numquam eveniet accusamus vitae? Nobis.
-            </p>
-          </div> -->
-        </div>
+        </section>
       </section>
-    </section>
+      <section
+        class="members-info absolute md:left-9 grid grid-cols-2
+         md1:grid-cols-2 max-w-sm md:max-w-xl lg:max-w-3xl p-6
+          mb-16 md:mb-8 bg-white border border-gray-200
+           rounded-lg shadow hover:bg-gray-100 glass"
+      >
+        <div class="col-span-1"></div>
+        <section class="col-span-1">
+          <div class="card">
+         
+  
+            <div id="izak-info" class="member">
+              <a href="#close"> X </a>
+              <h1>Izak Info</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+                velit magni illo deserunt iusto voluptatem rem cumque
+                necessitatibus eius tempora. Corporis, modi. Nisi quia iusto
+                mollitia ex porro esse recusandae?
+              </p>
+            </div>
+            <div id="sam-info" class="member">
+              <a href="#close"> X </a>
+              <h1>Sam Info</h1>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod
+                voluptatem deserunt hic maxime. Doloribus exercitationem
+                distinctio veritatis voluptatibus, adipisci amet possimus ullam
+                dolores facere rem numquam eveniet accusamus vitae? Nobis.
+              </p>
+            </div>
+          </div>
+        </section>
+      </section> -->
 
     <div
-      id="hire-us-info"
-      class="w-96 bg-purple-400 p-6 rounded-xl relative -top-[-20%] md:-top-[31rem] md:-left-48 min-h-full text-blue-700"
+      id="members-cards-info"
+      class="w-96 bg-purple-400 p-6 rounded-xl  relative 
+        min-h-full text-blue-700 z-[1] left-[-20%] md:left-[-11rem]"
     >
       <div class="card">
-        <header>Our Team members</header>
-        <section class="members">
+        <header class="text-2xl font-semibold">Our Team members</header>
+        <section class="members flex items-center justify-center gap-2">
           <div>
             <h2>Izak</h2>
             <a href="#izak-info">more info...</a>
@@ -74,6 +89,32 @@
         </section>
       </div>
     </div>
+
+    <section
+        class="contact-info absolute  grid grid-cols-2
+         md1:grid-cols-2 max-w-sm md:max-w-xl lg:max-w-3xl p-6
+          mb-16 md:mb-8 bg-white border border-gray-200
+           rounded-lg shadow hover:bg-gray-100 glass"
+      >
+        <div class="col-span-1"></div>
+        <section class="col-span-1">
+          <div class="card">
+            <div  class="font-normal text-gray-900">
+              <h2 class="text-center font-semibold text-2xl text-blue-700">
+                Contact Us to make your dream a reality
+              </h2>
+              <div class="flex flex-col">
+                <div>Tel: <br>
+                  <a href="tel:+1 (424)-442-0591">(424)-442-0591 </a></div>
+                <div>
+                  Email:
+                  <a href="mailto:pazpaz25@gmail.com">pazpaz25@gmail.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
   </section>
 </template>
 
@@ -100,7 +141,10 @@ export default {
 header h1 {
   animation: slide-in 0.8s ease-in-out;
 }
-
+.container {
+  
+  position: relative;
+}
 .glass {
   -webkit-backdrop-filter: blur(10px);
   backdrop-filter: blur(10px);
@@ -128,6 +172,7 @@ header h1 {
   position: absolute;
   top: 0;
   left: 0;
+  z-index: -1;
   height: 100vh;
   width: 100%;
   background-image: linear-gradient(
@@ -142,7 +187,7 @@ header h1 {
   background-repeat: no-repeat;
 }
 
-.card {
+#members-cards-info .card {
   width: calc(300px - 0px);
   height: 350px;
   display: flex;
@@ -152,12 +197,9 @@ header h1 {
   gap: 2rem;
 }
 
-.members {
-  display: flex;
-  gap: 1rem;
-}
+ 
 
-.card .members div {
+#members-cards-info  .members div {
   background: white;
   border: 1px solid gray;
   border-radius: 10%;
@@ -184,21 +226,14 @@ header h1 {
   transition: all 2s ease-in-out;
 } */
 
-#contact {
+#contact-info {
   opacity: 1;
   transform: translateX(0%);
   transition: transform 2s ease-in-out, opacity 2s ease-in-out;
   height: 100%;
   width: 100%;
 }
-/* .card:has(.member) .member{
-  opacity: 0;
-} */
-/* .hire-us-info:has(:target) #contact {
-  opacity: 0;
-  transform: translateX(-120%);
-}
-  */
+
 
 .card div a {
   cursor: pointer;
