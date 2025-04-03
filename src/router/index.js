355348fc64ca8e64fb0a8c71d42db2835b4bf887
Izak T <pazpaz25@gmail.com>
2025-04-03 12:36:10 -0700

    - Add navigation components and setup initial routing with works page - change my-work to our-works

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Blog from '../components/Blog.vue'
+import BlogPost from '../components/BlogPost.vue'
+import Works from '../views/Works.vue'
+import HireUs from '../views/HireUs.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/our-works',
+    name: 'Works',
+    component: Works
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
+  },
+  {
+    path: '/blog/:slug',
+    name: 'BlogPost',
+    component: BlogPost
+  },
+  {
+    path: '/hire-us',
+    name: 'HireUs',
+    component: HireUs
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
