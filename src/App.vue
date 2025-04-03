c1cc554c851663ece90b53c2a5cc5817cb9f96c7
Izak T <pazpaz25@gmail.com>
2025-04-03 10:32:29 -0700

    refactor: Clean up App.vue and implement RouterView
    - Removed unused component imports (Hero, Works, Contact)
    - Added RouterView to handle all route components
    - Simplified the template structure
    - Maintained essential components (Menu, GoBackTop)
    - Removed unnecessary style tag

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template lang="">
   <main class="overflow-scroll md:overflow-visible pb-[50px] md:pb-0">
-    <Menu></Menu>
-    <Hero />
-    <GoBackTop />
-    <Works  />
-    <Contact />
+    <Menu></Menu> 
+    <GoBackTop /> 
+    <RouterView />
   </main>
 </template>
 <script>
 import Menu from "./components/Menu.vue";
-import Hero from "./views/Hero.vue";
-import Works from "./views/Works.vue";
-import Contact from "./views/HireUs.vue";
 import GoBackTop from "./components/GoBackTop.vue";
 export default {
   components: {
     Menu,
-    Hero,
-    Works,
-    Contact,
     GoBackTop,
   },
 };
 </script>
-<style lang=""></style>
+
