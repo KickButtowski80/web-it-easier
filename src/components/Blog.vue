dbfbf45925bca5dd63bd07af1f8e2fa5fd9e55c7
Izak T <pazpaz25@gmail.com>
2025-04-04 08:46:53 -0700

    show the whole blog page that was hidden behind the top menu

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 py-8">
+  <div class="container mx-auto px-4 py-28">
     <h1 class="text-4xl font-bold mb-8">Blog</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <div v-for="post in posts" :key="post.slug" class="bg-white rounded-lg shadow-md overflow-hidden">
