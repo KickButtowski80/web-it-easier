53cf23cacd1291c8b69a1e05f504cd064a519bc0
Izak T <pazpaz25@gmail.com>
2025-04-24 19:36:51 -0700

    add universal skip to main content a tag

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
+  <a href="#main-content" class="sr-only focus-visible:not-sr-only focus-visible:fixed focus-visible:top-4 focus-visible:left-4
+             focus-visible:bg-white focus-visible:text-blue-500 focus-visible:py-2 focus-visible:px-4
+             focus-visible:rounded-md focus-visible:outline-none focus-visible:ring-2
+             focus-visible:ring-blue-500 focus-visible:ring-opacity-50 focus-visible:z-[9999]">
+    Skip to main content
+  </a>
+  <Menu />
+  <GoBackTop />
   <main class="overflow-scroll md:overflow-visible pb-[50px] md:pb-0">
-    <Menu></Menu> 
-    <GoBackTop /> 
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component" />
