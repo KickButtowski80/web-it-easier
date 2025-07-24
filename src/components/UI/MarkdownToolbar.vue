24611e93b2251077b8fba4153767dd88c69fec63
Izak T <pazpaz25@gmail.com>
2025-07-24 16:46:23 -0700

    add plus sing to bulled list item and change the title

--- src/components/UI/MarkdownToolbar.vue
+++ src/components/UI/MarkdownToolbar.vue
@@ -43,7 +43,8 @@
             </svg>
         </button>
         <div class="toolbar-divider"></div>
-        <button type="button" class="toolbar-btn" title="Bulleted List" @click="insertMarkdown('- ', '')">
+        <button type="button" class="toolbar-btn" title="Add Bulleted List Item" @click="insertMarkdown('- ', '')">
+            <span style="margin-right: 4px;">+</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <line x1="8" y1="6" x2="21" y2="6"></line>
